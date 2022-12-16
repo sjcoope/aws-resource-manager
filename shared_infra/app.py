30f26462c9f32d8456594a0026ccd62d95c20821
Simon Coope <sjcoope@amazon.com>
2022-12-16 16:55:41 +1100

    Added initial structure and EventBridge rule

--- shared_infra/app.py
+++ shared_infra/app.py
@@ -1,7 +1,12 @@
 import os
 import aws_cdk as cdk
-from aws_cdk import Stack
+from aws_cdk import (
+    aws_events as events,
+    aws_lambda as lambda_,
+    Stack, Duration
+)
 from constructs import Construct
+
 
 app = cdk.App()
 
@@ -11,6 +16,26 @@ class ResourceManagerCoreStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        rule = events.Rule(
+                self, "ResourceManagerSweepStarted",
+                schedule=events.Schedule.cron(
+                    minute='0',
+                    hour='18',
+                    month='*',
+                    week_day='MON-FRI',
+                    year='*')
+            )
+        
+        # Sweep Function
+        #lambdaFn = lambda_.Function(
+        #)
+        # lambdaFn = pyLambda.PythonFunction(self, 
+        #     "ResourceManager-Sweep",
+        #     code=lambda_.Code.from_asset('./sweeper'),
+        #     handler="sweeper.handler",
+        #     timeout=Duration.seconds(300),
+        #     runtime=lambda_.Runtime.PYTHON_3_9,  
+        # )
 
 ResourceManagerCoreStack(app, "ResourceManager-Shared-Infra",
     env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
